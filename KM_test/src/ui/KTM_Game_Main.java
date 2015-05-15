be1f831f1fc81cb2b4fb9ea9a4fd242b2156261a
husten1997 <julian@dietlmeiers.de>
2015-05-15 14:28:17 +0200

    -implemented Fullscreen

--- KM_test/src/ui/KTM_Game_Main.java
+++ KM_test/src/ui/KTM_Game_Main.java
@@ -34,7 +34,7 @@ public class KTM_Game_Main implements Strings{
     
     public static int WIDTH = 1600;
     public static int HEIGHT = 900;
-    
+    private boolean fS = true; //fullscreen?
     int delta;
 
     Soldat figur;
@@ -133,6 +133,7 @@ public class KTM_Game_Main implements Strings{
 	    
 	    getDelta(); // call once before loop to initialise lastFrame
 	    lastFPS = getTime(); // call before loop to initialise fps timer
+	    setDisplayMode(WIDTH, HEIGHT, fS);
 	}
 
 	public void initRender(gasset input, int e, int se){
@@ -196,7 +197,7 @@ public class KTM_Game_Main implements Strings{
 		}
 		
 		if(Keyboard.isKeyDown(Keyboard.KEY_Q)){
-			
+			setDisplayMode(WIDTH, HEIGHT, !fS);
 		}
 		
 		if(Keyboard.isKeyDown(Keyboard.KEY_UP)){
@@ -437,6 +438,62 @@ public class KTM_Game_Main implements Strings{
 			System.out.println(CalV.get(i).getO().getPos().getPos());
 		}
 	}
+	
+	public void setDisplayMode(int width, int height, boolean fullscreen) {
+		 
+        // return if requested DisplayMode is already set
+                if ((Display.getDisplayMode().getWidth() == width) && 
+            (Display.getDisplayMode().getHeight() == height) && 
+            (Display.isFullscreen() == fullscreen)) {
+            return;
+        }
+         
+        try {
+            DisplayMode targetDisplayMode = null;
+             
+            if (fullscreen) {
+                DisplayMode[] modes = Display.getAvailableDisplayModes();
+                int freq = 0;
+                 
+                for (int i=0;i<modes.length;i++) {
+                    DisplayMode current = modes[i];
+                     
+                    if ((current.getWidth() == width) && (current.getHeight() == height)) {
+                        if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+                            if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+                                targetDisplayMode = current;
+                                freq = targetDisplayMode.getFrequency();
+                            }
+                        }
+ 
+                        // if we've found a match for bpp and frequence against the 
+                        // original display mode then it's probably best to go for this one
+                        // since it's most likely compatible with the monitor
+                        if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
+                            (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+                            targetDisplayMode = current;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                targetDisplayMode = new DisplayMode(width,height);
+            }
+             
+            if (targetDisplayMode == null) {
+                System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
+                return;
+            }
+ 
+            Display.setDisplayMode(targetDisplayMode);
+            Display.setFullscreen(fullscreen);
+            fS = fullscreen;
+            
+             
+        } catch (LWJGLException e) {
+            System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+        }
+    }
 
 	public float getH(){
 		return HEIGHT;
