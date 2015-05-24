3de938e1c86086b2b3775564d77f592efe7dff13
Richard Kellnberger <Richard1.Kellnberger@web.de>
2015-05-24 15:06:30 +0200

    Verschönerungen

--- KM_test/src/com/husten/knightmare/constants/StringConstants.java
+++ KM_test/src/com/husten/knightmare/constants/StringConstants.java
@@ -10,13 +10,13 @@ public interface StringConstants {
 		public static String MOOR = "MOOR";
 		public static String FLESH = "FLESH";
 	}
-	
+
 	public interface MeshType {
 		public static String EINEHEIT = "EINHEIT";
 		public static String GEBÄUDE = "GEBÄUDE";
 		public static String GROUND = "GROUND";
 	}
-	
+
 	public interface state {
 		public static String NOTHING = "NOTHING";
 		public static String S_BUILDINGS = "S_BUILDINGS";
--- KM_test/src/com/husten/knightmare/worldGen/WorldGen.java
+++ KM_test/src/com/husten/knightmare/worldGen/WorldGen.java
@@ -6,60 +6,50 @@ import com.husten.knightmare.constants.StringConstants;
 import com.husten.knightmare.graphicalObjects.Terrain;
 import com.husten.knightmare.graphicalObjects.TerrainElement;
 import com.husten.knightmare.graphicalObjects.TextureLoader;
-import com.husten.knightmare.worldGen.*;
 import com.richard.knightmare.util.Vektor;
 
-public class WorldGen implements StringConstants{
-	
-	static final String RAND = "RAND";
-	static final String RAND_DQ = "RAND_DQ";
-	static final String RAND_DD = "RAND_DD";
-	static final String SIN = "SIN";
-	
-	TerrainElement World[][];
-	Terrain Terrain;
-	TextureLoader tL;
-	
-	int x;
-	int y;
-	
-	double lW = 0.58;
-	double lS = 0.61;
-	double lG = 0.81;
-	double lR = 1;
-	
-	float max;
-	float min;
-	float dif;
-	
-	float[][] hm;
-	
-	float routh = 0.6f;
-	float fallof = 0.8f;
-	float c = 2f;
-	
-	static int seed = 1005464490;
-	int smoothS = 71;
-	Random rand; 
-	Generator generator;
-	
-	public WorldGen(TerrainElement world[][], Terrain terrain){
+public class WorldGen implements StringConstants {
+
+	private static final String RAND = "RAND", RAND_DQ = "RAND_DQ", RAND_DD = "RAND_DD", SIN = "SIN";
+
+	private TerrainElement World[][];
+	private Terrain Terrain;
+	private TextureLoader tL;
+
+	private int x, y, smoothS = 71;
+
+	private double lW = 0.58, lS = 0.61, lG = 0.81, lR = 1;
+
+	private float max, min, dif;
+
+	private float[][] hm;
+
+	private float routh = 0.6f, fallof = 0.8f, c = 2f;
+
+	private static int seed = 1005464490;
+	private Random rand;
+	private Generator generator;
+
+	public WorldGen(TerrainElement world[][], Terrain terrain) {
 		World = world;
 		Terrain = terrain;
 		tL = Terrain.getTextureLoader();
 		x = Terrain.getSx();
 		y = Terrain.getSy();
 		hm = new float[x][y];
-		
+
 	}
-	
-	public TerrainElement[][] worldGen(){
+
+	public TerrainElement[][] worldGen() {
 		gen();
 		return World;
 	}
-/**Die auskomentierten Variablen sind mit absicht drinnen da man mithilfe von time die generierungsdauer auslesen kann!
- */
-	public void gen(){
+
+	/**
+	 * Die auskomentierten Variablen sind mit absicht drinnen da man mithilfe
+	 * von time die generierungsdauer auslesen kann!
+	 */
+	public void gen() {
 		rand = new Random(seed);
 		generator = new RandomGenerator(rand, c, -1);
 		long Zvor = System.currentTimeMillis();
@@ -67,150 +57,146 @@ public class WorldGen implements StringConstants{
 		set();
 		trans();
 		long Znach = System.currentTimeMillis();
-		long time = Znach-Zvor;
+		long time = Znach - Zvor;
 		System.out.println("Done in: " + time + "ms");
 	}
-	
-	public void genHM( int seed, float r){
-		int inter = Math.round(x/2);
-		square(inter+1, inter+1, inter, r);
+
+	public void genHM(int seed, float r) {
+		int inter = Math.round(x / 2);
+		square(inter + 1, inter + 1, inter, r);
 		smooth(smoothS);
 	}
-	
-	
-	public void set(){
+
+	public void set() {
 		min = hm[1][1];
 		max = hm[1][1];
-		for(int i = 0; i < hm.length; i++){
-			for(int j = 0; j < hm.length; j++){
-				min = (hm[i][j] < min ? hm[i][j]:min);
-				max = (hm[i][j] > max ? hm[i][j]:max);
+		for (int i = 0; i < hm.length; i++) {
+			for (int j = 0; j < hm.length; j++) {
+				min = (hm[i][j] < min ? hm[i][j] : min);
+				max = (hm[i][j] > max ? hm[i][j] : max);
 			}
 		}
 		dif = max - min;
 	}
-	
-	public void trans(){
+
+	public void trans() {
 		double WW = min + (lW * dif);
 		double WS = min + (lS * dif);
 		double WG = min + (lG * dif);
 		double WR = min + (lR * dif);
-		for(int i = 0; i < hm.length; i++){
-			for(int j = 0; j < hm.length; j++){
+		for (int i = 0; i < hm.length; i++) {
+			for (int j = 0; j < hm.length; j++) {
 				float z = hm[i][j];
-				
-				if(z < WW){
+
+				if (z < WW) {
 					World[i][j] = null;
 				}
-				if(z > WW && z < WS){
-					World[i][j] = new TerrainElement(32,32,i*32,j*32, tL, "sand.png", Material.SAND);
+				if (z > WW && z < WS) {
+					World[i][j] = new TerrainElement(32, 32, i * 32, j * 32, tL, "sand.png", Material.SAND);
 				}
-				if(z > WS && z < WG){
-					World[i][j] = new TerrainElement(32,32,i*32,j*32, tL, "gras.png", Material.GRAS);
+				if (z > WS && z < WG) {
+					World[i][j] = new TerrainElement(32, 32, i * 32, j * 32, tL, "gras.png", Material.GRAS);
 				}
-				if(z > WG && z < WR){
-					World[i][j] = new TerrainElement(32,32,i*32,j*32, tL, "rock.png", Material.ROCK);
+				if (z > WG && z < WR) {
+					World[i][j] = new TerrainElement(32, 32, i * 32, j * 32, tL, "rock.png", Material.ROCK);
 				}
 			}
 		}
 	}
-	
-	public void square(int xPos, int yPos, int inter, float r){
-		if(xPos < x && xPos >= 0 && yPos < y && yPos >= 0){
-			if(hm[xPos][yPos] == 0.0){
-					hm[xPos][yPos] = generator.getValue(xPos, yPos, r, dsquare(xPos, yPos, inter));
+
+	public void square(int xPos, int yPos, int inter, float r) {
+		if (xPos < x && xPos >= 0 && yPos < y && yPos >= 0) {
+			if (hm[xPos][yPos] == 0.0) {
+				hm[xPos][yPos] = generator.getValue(xPos, yPos, r, dsquare(xPos, yPos, inter));
 			}
 			diamond(xPos + inter, yPos, inter, r);
 			diamond(xPos, yPos + inter, inter, r);
 			diamond(xPos - inter, yPos, inter, r);
-			diamond(xPos, yPos- inter, inter, r);
+			diamond(xPos, yPos - inter, inter, r);
 		}
 	}
-	
-	public void diamond(int xPos, int yPos, int inter, float r){
-		if(inter >= 1 && xPos < x && xPos >= 0 && yPos < y && yPos >= 0){
-			if(hm[xPos][yPos] == 0.0){
+
+	public void diamond(int xPos, int yPos, int inter, float r) {
+		if (inter >= 1 && xPos < x && xPos >= 0 && yPos < y && yPos >= 0) {
+			if (hm[xPos][yPos] == 0.0) {
 				hm[xPos][yPos] = generator.getValue(xPos, yPos, r, ddia(xPos, yPos, inter));
-				
-				Vektor M1 = middl(new Vektor((double)(xPos + inter),(double) (yPos)), new Vektor((double)(xPos), (double)(yPos + inter)));
-				square((int)M1.getX() + (inter/2), (int)M1.getY() + (inter/2), (int) Math.round(inter/2), r*fallof);
 
-				Vektor M2 = middl(new Vektor((double)(xPos - inter), (double)(yPos)), new Vektor((double)(xPos), (double)(yPos + inter)));
-				square((int)M2.getX() + (inter/2), (int)M2.getY() + (inter/2), (int) Math.round(inter/2), r*fallof);
+				Vektor M1 = middl(new Vektor((double) (xPos + inter), (double) (yPos)), new Vektor((double) (xPos), (double) (yPos + inter)));
+				square((int) M1.getX() + (inter / 2), (int) M1.getY() + (inter / 2), (int) Math.round(inter / 2), r * fallof);
 
-				Vektor M3 = middl(new Vektor((double)(xPos + inter), (double)(yPos)), new Vektor((double)(xPos), (double)(yPos - inter)));
-				square((int)M3.getX() + (inter/2), (int)M3.getY() + (inter/2), (int) Math.round(inter/2), r*fallof);
+				Vektor M2 = middl(new Vektor((double) (xPos - inter), (double) (yPos)), new Vektor((double) (xPos), (double) (yPos + inter)));
+				square((int) M2.getX() + (inter / 2), (int) M2.getY() + (inter / 2), (int) Math.round(inter / 2), r * fallof);
 
-				Vektor M4 = middl(new Vektor((double)(xPos - inter), (double)(yPos)), new Vektor((double)(xPos), (double)(yPos - inter)));
-				square((int)M4.getX() + (inter/2), (int)M4.getY() + (inter/2), (int) Math.round(inter/2), r*fallof);
+				Vektor M3 = middl(new Vektor((double) (xPos + inter), (double) (yPos)), new Vektor((double) (xPos), (double) (yPos - inter)));
+				square((int) M3.getX() + (inter / 2), (int) M3.getY() + (inter / 2), (int) Math.round(inter / 2), r * fallof);
+
+				Vektor M4 = middl(new Vektor((double) (xPos - inter), (double) (yPos)), new Vektor((double) (xPos), (double) (yPos - inter)));
+				square((int) M4.getX() + (inter / 2), (int) M4.getY() + (inter / 2), (int) Math.round(inter / 2), r * fallof);
 			}
-		}else{
+		} else {
 
 		}
-		
-		
-		
+
 	}
-	
-	public Vektor middl(Vektor v1, Vektor v2){
-		double x1 = (v1.getX() + v2.getX())/2;
-		double y1 = (v1.getY() + v2.getY())/2;
+
+	public Vektor middl(Vektor v1, Vektor v2) {
+		double x1 = (v1.getX() + v2.getX()) / 2;
+		double y1 = (v1.getY() + v2.getY()) / 2;
 		return new Vektor(x1, y1);
 	}
-	
-	public float ddia(int xPos, int yPos, int inter){
+
+	public float ddia(int xPos, int yPos, int inter) {
 		int blub = 0;
 		float value = 0;
-		if(xPos + inter < x){
+		if (xPos + inter < x) {
 			value += hm[xPos + inter][yPos];
 			blub++;
 		}
-		if(xPos - inter > 0){
+		if (xPos - inter > 0) {
 			value += hm[xPos - inter][yPos];
 			blub++;
 		}
-		if(yPos + inter < y){
+		if (yPos + inter < y) {
 			value += hm[xPos][yPos + inter];
 			blub++;
 		}
-		if(yPos - inter > 0){
+		if (yPos - inter > 0) {
 			value += hm[xPos][yPos - inter];
 			blub++;
 		}
-		return value/blub;
+		return value / blub;
 	}
-	
-	public float dsquare(int xPos, int yPos, int inter){
+
+	public float dsquare(int xPos, int yPos, int inter) {
 		int blub = 0;
 		float value = 0;
-		if(xPos + inter < x && yPos + inter < y){
+		if (xPos + inter < x && yPos + inter < y) {
 			value += hm[xPos + inter][yPos + inter];
 			blub++;
 		}
-		if(xPos - inter >= 0 && yPos - inter >= 0){
+		if (xPos - inter >= 0 && yPos - inter >= 0) {
 			value += hm[xPos - inter][yPos - inter];
 			blub++;
 		}
-		if(xPos - inter >= 0 && yPos + inter < y){
+		if (xPos - inter >= 0 && yPos + inter < y) {
 			value += hm[xPos - inter][yPos + inter];
 			blub++;
 		}
-		if(xPos + inter < x && yPos - inter >= 0){
+		if (xPos + inter < x && yPos - inter >= 0) {
 			value += hm[xPos + inter][yPos - inter];
 			blub++;
 		}
-		return value/blub;
+		return value / blub;
 	}
-	
-	public void smooth(int am){
-		for(int s = 0; s < am; s++){
-			for(int i = 0; i < hm.length; i++){
-				for(int j = 0; j < hm.length; j++){
+
+	public void smooth(int am) {
+		for (int s = 0; s < am; s++) {
+			for (int i = 0; i < hm.length; i++) {
+				for (int j = 0; j < hm.length; j++) {
 					hm[i][j] = ddia(i, j, 1);
 				}
 			}
 		}
 	}
-	
-}
 
+}
