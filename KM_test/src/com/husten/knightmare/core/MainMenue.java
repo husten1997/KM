694772cbf6f90b1a49361c9dfc9d66b421441c0e
Richard Kellnberger <Richard1.Kellnberger@web.de>
2015-05-26 16:13:38 +0200

    Spiel starten

--- KM_test/src/com/husten/knightmare/core/MainMenue.java
+++ KM_test/src/com/husten/knightmare/core/MainMenue.java
@@ -1,9 +1,26 @@
 package com.husten.knightmare.core;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class MainMenue {
 
 	public static void main(String[] args) {
-		new MainMenue().spielStarten();
+		JFrame f = new JFrame("Ähh");
+		JButton start = new JButton("Start");
+		f.add(start);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		start.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				new MainMenue().spielStarten();
+			}
+		});
 	}
 	
 	private void spielStarten(){
