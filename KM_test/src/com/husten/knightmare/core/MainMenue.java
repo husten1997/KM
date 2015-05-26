1e13cd26f12add4447face9cb4a55914052b32da
Richard Kellnberger <Richard1.Kellnberger@web.de>
2015-05-26 15:57:14 +0200

    ged wieda

--- KM_test/src/com/husten/knightmare/core/MainMenue.java
+++ KM_test/src/com/husten/knightmare/core/MainMenue.java
@@ -1,15 +1,9 @@
 package com.husten.knightmare.core;
 
-import org.lwjgl.input.Keyboard;
-
 public class MainMenue {
 
 	public static void main(String[] args) {
-		while(true){
-			if(Keyboard.getEventKey() == Keyboard.KEY_S){
-				new MainMenue().spielStarten();
-			}
-		}
+		new MainMenue().spielStarten();
 	}
 	
 	private void spielStarten(){
