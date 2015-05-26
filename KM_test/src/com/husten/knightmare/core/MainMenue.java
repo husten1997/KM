41ccada98f3feddf84f75fccdf5996df2f148976
husten1997 <julian@dietlmeiers.de>
2015-05-26 17:50:06 +0200

    Improovements

--- KM_test/src/com/husten/knightmare/core/MainMenue.java
+++ KM_test/src/com/husten/knightmare/core/MainMenue.java
@@ -57,18 +57,7 @@ public class MainMenue {
 	
 
 	public static void main(String[] args) {
-		JFrame f = new JFrame("Ähh");
-		JButton start = new JButton("Start");
-		f.add(start);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		start.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
 				new MainMenue().spielStarten();
-			}
-		});
 	}
 	
 	private void spielStarten(){
