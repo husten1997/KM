be24b0b491449661ac6e902041546b48c11691c9
THEGDK <matthias-kettl@gmx.de>
2015-05-29 14:33:00 +0200

    Laden

--- KM_test/src/com/husten/knightmare/core/MainMenueJFrame.java
+++ KM_test/src/com/husten/knightmare/core/MainMenueJFrame.java
@@ -86,8 +86,8 @@ public class MainMenueJFrame extends JFrame {
 		buttons.add(new Button(new Pos(w(848)*width, h(751)*height), new Pos(width, h(838)*height)) {
 			@Override
 			public void onClick() {
-				dispose();
-				new Knightmare();
+				new Laden(w,h,isUndecorated(),mm);
+				setVisible(false);
 			}
 		});
 		
