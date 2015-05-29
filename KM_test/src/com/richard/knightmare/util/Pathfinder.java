c96e5c0214d44a2f928e6eed38530a38a28f3084
Richard Kellnberger <Richard1.Kellnberger@web.de>
2015-05-29 11:54:22 +0200

    Pathfinder

---
+++ KM_test/src/com/richard/knightmare/util/Pathfinder.java
@@ -0,0 +1,14 @@
+package com.richard.knightmare.util;
+
+import java.util.ArrayList;
+
+import com.husten.knightmare.graphicalObjects.GraphicalObject;
+
+public class Pathfinder {
+
+	private ArrayList<Pos> way = new ArrayList<>();
+	
+	public Pathfinder(GraphicalObject[][] world, Pos start, Pos ziel){
+		
+	}
+}
