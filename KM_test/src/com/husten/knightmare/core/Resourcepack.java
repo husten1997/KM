aa8a73b746ad240426437ef95da17fd01604d9a0
THEGDK <matthias-kettl@gmx.de>
2015-05-30 22:21:29 +0200

    Default Texturepack Abfrage

--- KM_test/src/com/husten/knightmare/core/Resourcepack.java
+++ KM_test/src/com/husten/knightmare/core/Resourcepack.java
@@ -19,6 +19,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -26,22 +27,39 @@ import javax.swing.event.ListSelectionListener;
 import com.richard.knightmare.util.Loader;
 
 @SuppressWarnings("serial")
-public class Resourcepack extends JFrame implements ListSelectionListener, KeyListener, ActionListener {
+public class Resourcepack extends JFrame implements ListSelectionListener,
+		KeyListener, ActionListener {
 
 	private JList<String> list;
 	private File path;
 	private String[] text;
 	private Optionen op;
 	private JButton zurück;
+	private String fehler;
 
 	public Resourcepack(Optionen o) {
 		Loader.initLoaderWithoutLoad("Ares", "Knightmare");
 
 		op = o;
+		fehler = "Resourcepack kann nicht 'Default' heißen";
 
-		path = new File(
-				new StringBuilder("C:\\Users\\").append(System.getProperty("user.name")).append("\\Appdata\\Roaming\\ares\\Knightmare\\resourcepacks").toString());
-		text = path.list();
+		path = new File(new StringBuilder("C:\\Users\\")
+				.append(System.getProperty("user.name"))
+				.append("\\Appdata\\Roaming\\ares\\Knightmare\\resourcepacks")
+				.toString());
+		text = new String[path.list().length + 1];
+
+		for (int i = 0; i < text.length; i++) {
+			if (i == 0) {
+				text[i] = "Default";
+			} else {
+				if (path.list()[i - 1].equals("Default")) {
+					text[i] = fehler;
+				} else {
+					text[i] = path.list()[i - 1];
+				}
+			}
+		}
 
 		double resolution = (double) 16 / (double) 9;
 		int width, height;
@@ -73,13 +91,16 @@ public class Resourcepack extends JFrame implements ListSelectionListener, KeyLi
 		setUndecorated(Loader.getCfgValue("Fullscreen").equals("true"));
 		setVisible(true);
 
-		BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		img.getGraphics().drawImage(Loader.getImage("back.png"), 0, 0, width, height, null);
+		BufferedImage img = new BufferedImage(width, height,
+				BufferedImage.TYPE_INT_ARGB);
+		img.getGraphics().drawImage(Loader.getImage("back.png"), 0, 0, width,
+				height, null);
 		setContentPane(new JLabel(new ImageIcon(img)));
 
 		list = new JList<String>(text); // data has type Object[]
 		list.setSize(new Dimension(width, height));
-		list.setBounds((screen.width - width) / 2 + width / 4, (screen.height - height) / 2, width / 2, height);
+		list.setBounds((screen.width - width) / 2 + width / 4,
+				(screen.height - height) / 2, width / 2, height);
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setBackground(new Color(0, 0, 0.25f, 0.25f));
 		list.setForeground(Color.white);
@@ -96,12 +117,15 @@ public class Resourcepack extends JFrame implements ListSelectionListener, KeyLi
 		}
 
 		add(list);
-		((DefaultListCellRenderer) list.getCellRenderer()).setHorizontalAlignment(JLabel.CENTER);
+		((DefaultListCellRenderer) list.getCellRenderer())
+				.setHorizontalAlignment(JLabel.CENTER);
 
 		zurück = new JButton("Zurück");
 		zurück.setBackground(new Color(0.5f, 0.5f, 0.5f, 0.5f));
 		zurück.setFont(new Font("Arial", Font.BOLD, width / 48));
-		zurück.setBounds(screen.width / 2 + 3 * width / 8, (screen.height - height) / 2 + height - width / 24, width / 8, width / 24);
+		zurück.setBounds(screen.width / 2 + 3 * width / 8,
+				(screen.height - height) / 2 + height - width / 24, width / 8,
+				width / 24);
 		zurück.addActionListener(this);
 		zurück.setRolloverEnabled(false);
 		zurück.setFocusable(false);
@@ -117,11 +141,15 @@ public class Resourcepack extends JFrame implements ListSelectionListener, KeyLi
 
 				@Override
 				public void mouseReleased(MouseEvent e) {
-					if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 2) {
-						Loader.changeCfgValue("Resourcepack", text[list.getSelectedIndex()]);
-						op.setVisible(true);
-						op.setAutoRequestFocus(true);
-						dispose();
+					if (e.getButton() == MouseEvent.BUTTON1
+							&& e.getClickCount() == 2) {
+						if (!text[list.getSelectedIndex()].equals(fehler)) {
+							Loader.changeCfgValue("Resourcepack",
+									text[list.getSelectedIndex()]);
+							op.setVisible(true);
+							op.setAutoRequestFocus(true);
+							dispose();
+						}
 					}
 
 				}
@@ -156,7 +184,7 @@ public class Resourcepack extends JFrame implements ListSelectionListener, KeyLi
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == zurück){
+		if (e.getSource() == zurück) {
 			op.setVisible(true);
 			op.setAutoRequestFocus(true);
 			dispose();
