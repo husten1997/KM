7c205bb4695e07cb36f833a92bde805a28634c64
THEGDK <matthias-kettl@gmx.de>
2015-05-30 14:17:59 +0200

    Weil ich nicht der hellste bin

--- KM_test/src/com/husten/knightmare/core/Laden.java
+++ KM_test/src/com/husten/knightmare/core/Laden.java
@@ -72,7 +72,7 @@ public class Laden extends JFrame implements ListSelectionListener {
 				mm.setVisible(true);
 				dispose();
 			} else {
-				//TODO load(list.getSelectedIndex());
+				//TODO load(list.getSelectedIndex());ü
 			}
 		}
 
