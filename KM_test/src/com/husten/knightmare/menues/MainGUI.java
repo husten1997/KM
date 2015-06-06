fe0dc34f8a01322ed38a0c5e70f646eee28397af
husten1997 <julian@dietlmeiers.de>
2015-06-06 21:36:38 +0200

    #ichblicknichtganzdurch

--- KM_test/src/com/husten/knightmare/menues/MainGUI.java
+++ KM_test/src/com/husten/knightmare/menues/MainGUI.java
@@ -1,19 +1,31 @@
 package com.husten.knightmare.menues;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import org.lwjgl.LWJGLException;
 
 import de.matthiasmann.twl.*;
 import de.matthiasmann.twl.renderer.lwjgl.LWJGLRenderer;
+import de.matthiasmann.twl.theme.ThemeManager;
 
 public class MainGUI extends Widget {
 	
 	private LWJGLRenderer renderer;
+	private GUI gui;
 	
-	public MainGUI(Widget rootPane) throws LWJGLException{
+	public MainGUI(Widget rootPane) throws LWJGLException, MalformedURLException, IOException{
 			renderer = new LWJGLRenderer();
-			GUI gui = new GUI(rootPane, renderer);
+			gui = new GUI(rootPane, renderer);
+			ThemeManager theme = ThemeManager.createThemeManager(new File("gameui.xml").toURL(), renderer);
+			//Probleme mit dem xml-File #ichsteignichtganzdurch
 			
 			
+	}
+	
+	public void update(){
+		gui.update();
 	}
 
 }
